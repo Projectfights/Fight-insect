b91682f284943b320c9040f60a5906853b484c9b
Henry Conkin <hwc5gj@virginia.edu>
2014-09-22 14:06:56 -0400

    Revert "Revert "Added Input library""
    This reverts commit fc92d118eab31c3a307ae501b2777689a8f1b6fc.

---
+++ SuperBugFighter/InputListener/ScrollEvent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InputListener
+{
+    class ScrollEvent : Event
+    {
+        public int Scroll { get; private set; }
+
+        public ScrollEvent(int type, Point screenLocation, Point worldLocation, int scroll)
+            : base(type, screenLocation, worldLocation)
+        {
+            Scroll = scroll;
+        }
+    }
+}
