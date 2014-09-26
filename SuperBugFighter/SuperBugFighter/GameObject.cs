54142fcb982a24508bd374827dafea6a699d27f8
Henry Conkin <hwc5gj@virginia.edu>
2014-09-26 17:04:26 -0400

    Added GameObject class

---
+++ SuperBugFighter/SuperBugFighter/GameObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Bug
+{
+    public abstract class GameObject
+    {
+        protected Vector2 pos;
+
+        public GameObject()
+        {
+            this.pos = new Vector2();
+        }
+
+        public GameObject(Vector2 pos)
+        {
+            this.pos = pos;
+        }
+
+        public abstract void Update(long timePassed);
+
+        public abstract void Draw(SpriteBatch sBatch);
+
+
+	    
+    }
+}
