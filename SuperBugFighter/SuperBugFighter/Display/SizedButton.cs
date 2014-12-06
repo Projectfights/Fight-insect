bdfe06879bc462c8efa1015bb881279505a7a599
Henry Conkin <hwc5gj@virginia.edu>
2014-12-05 23:39:22 -0500

    Working on Select Screen

---
+++ SuperBugFighter/SuperBugFighter/Display/SizedButton.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bug.Display
+{
+    class SizedButton : Button
+    {
+        private Rectangle bounds;
+
+        public SizedButton(Rectangle bounds, Texture2D up, Texture2D down) : base(x,y,up,down)
+        {
+
+        }
+
+        public override bool Collide(int pointX, int pointY)
+        {
+            return bounds.Contains(bounds.X, bounds.Y);
+        }
+
+        public override void Draw(SpriteBatch batch)
+        {
+            batch.Draw(tex, bounds, Color.White);
+        }
+    }
+}
---
+++ SuperBugFighter/SuperBugFighter/Screens/Concrete/SelectScreen.cs
@@ -0,0 +1,17 @@
+﻿using Bug.Screens.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bug.Screens.Concrete
+{
+    class SelectScreen : MenuScreen
+    {
+        public SelectScreen(int width, int height, IScreenMaster master)
+            : base(width, height, master)
+        {
+            
+        }
+    }
+}
